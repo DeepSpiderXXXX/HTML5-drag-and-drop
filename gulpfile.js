b366a9e06c143fff624e9f92b6bc89f38a2ee6b2
vincent-xw <jiaxuewen2006@163.com>
2017-07-19 18:35:20 +0800

    init project

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+
+gulp.task('webserver', function(){
+	// 监听文件变动
+gulp.watch(['source/js/**/*.js']);
+gulp.watch(['source/css/**/*.css']);
+gulp.watch(['source/tpl/**/*.html']);
+// 配置web服务器
+gulp.src('./source')
+  .pipe(webserver({
+    host: '0.0.0.0',
+    port: '8000',
+    livereload: true,
+    fallback: 'index.html'
+  }));
